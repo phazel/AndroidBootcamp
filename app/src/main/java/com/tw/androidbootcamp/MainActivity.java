ac7637eb396276162d2976772ed7387f3ad902b1
phazel <cwebster@thoughtworks.com>
2014-08-21 19:31:50 +1000

    stuff

--- app/src/main/java/com/tw/androidbootcamp/MainActivity.java
+++ app/src/main/java/com/tw/androidbootcamp/MainActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayDeque;
 
 
 public class MainActivity extends Activity {
@@ -28,6 +34,24 @@ public class MainActivity extends Activity {
                 textView.setBackgroundColor(Color.BLUE);
                 Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final String[] values = new String[] {"Android", "iPhone", "WindowsMobile"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, R.layout.list_item, R.id.item, values);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,  View view, int i, long l) {
+                Intent intent =  new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("TEST", values[i]);
+                Log.d("AAAAAA", values[i]);
+                startActivity(intent);
+
             }
         });
 
